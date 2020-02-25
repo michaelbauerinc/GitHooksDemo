f1411827d0dcf963a938bdef37e4a02e07d8b179
Michael Bauer <michael@claimfound.com>
2020-02-25 12:58:11 -0500

    Format black

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,13 @@
 def hello_world():
-    hello_dict = {"Hello": "World", "Hello": "World", "Hello": "World", "Hello": "World", "Hello": "World", "Hello": "World"}
+    hello_dict = {
+        "Hello": "World",
+        "Hello": "World",
+        "Hello": "World",
+        "Hello": "World",
+        "Hello": "World",
+        "Hello": "World",
+    }
     print(hello_dict)
+
+
+hello_world()
